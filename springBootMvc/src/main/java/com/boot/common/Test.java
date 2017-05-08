00f90c176f9fa3a1ebd83cbedd0bfb3dabf19154
HaultHuang <1032774144@qq.com>
2017-05-08 16:29:41 +0800

    Test commit

--- springBootMvc/src/main/java/com/boot/common/Test.java
+++ springBootMvc/src/main/java/com/boot/common/Test.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  */
 public class Test {
     public static void main(String[] args) {
-        String a = new BCryptPasswordEncoder().encode("user");
+        String a = new BCryptPasswordEncoder().encode("user11");
         System.out.printf(a);
     }
 }
